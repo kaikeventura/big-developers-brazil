b6d5f17ad42bcd7c129543be4b19e8d1d46e1d26
kaikeventura <kaike.gnr@hotmail.com>
2020-09-09 00:41:52 -0300

    Ajustes nas configurações do serviço de SNS

--- src/main/java/br/com/kaikeventura/bdb/config/S3Config.java
+++ src/main/java/br/com/kaikeventura/bdb/config/S3Config.java
@@ -8,6 +8,7 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class S3Config {
@@ -24,6 +25,7 @@ public class S3Config {
     @Value("${aws.region}")
     private String region;
 
+    @Primary
     @Bean
     public AmazonS3 s3client() {
         return AmazonS3ClientBuilder.standard()
